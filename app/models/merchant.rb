b6ebdcceda2d11ad910cdf5fd1ec6c1e2f320cb0
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-29 11:23:18 -0600

    Update the relationships.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,8 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
-  has_many :items, dependent: :delete_all
-  has_many :invoices, dependent: :delete_all
+  has_many :items, dependent: :destroy
+  has_many :invoices, dependent: :destroy
+
+  has_many :invoice_items, through: :invoices
   has_many :customers, through: :invoices
 end
