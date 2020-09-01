fc4d809bb2c06eda3e7fe3398ed6115d8b8b73e5
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-31 21:16:45 -0600

    Add find tests for merchant model.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :invoices}
     it { should have_many(:customers).through(:invoices)}
   end
+
+  describe 'methods' do
+    before(:each) do
+      create(:merchant, name: 'Korra Inc')
+      create(:merchant, name: 'Rokku Inc')
+      create(:merchant, name: 'Kyoshi Inc')
+      create(:merchant, name: 'Aang Inc')
+    end
+    it '.find_match' do
+      data = Merchant.find_match({"name": "k"})
+      expect(data.name).to eq("Korra Inc")
+    end
+
+    it '.find_all' do
+      data = Merchant.find_all({"name": "k"})
+      expect(data.size).to eq(3)
+    end
+  end
 end
