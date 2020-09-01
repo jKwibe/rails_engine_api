321f52673c64b9d8e679c9208b2dff76fd46133b
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-31 20:35:26 -0600

    Add methods to query for the database.

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,12 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
+
+  def self.find_match(query_params)
+    where("lower(#{query_params.keys.first}) LIKE ?", "%#{query_params.values.first.downcase}%").first
+  end
+
+  def self.find_all(query_params)
+    where("lower(#{query_params.keys.first}) LIKE ?", "%#{query_params.values.first.downcase}%")
+  end
 end
