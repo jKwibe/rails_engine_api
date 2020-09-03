ce419c5ea8727ce459838cff1d72c12100fbc2d7
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-02 22:12:17 -0600

    Add sad paths for index controller.

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   def index
-    merchant = Merchant.find(params[:id])
-    render json: ItemSerializer.new(merchant.items), status: :ok
+    begin
+      merchant = Merchant.find(params[:id])
+      render json: ItemSerializer.new(merchant.items), status: :ok
+    rescue => error
+      render json: { error: { message: error.message, status: 404 } }, status: :not_found
+    end
   end
 end
