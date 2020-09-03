40b67c3e144376214d2374151dc3e23133514eb1
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-02 20:37:13 -0600

    Add factories

---
+++ spec/factories/invoice_items.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :invoice_item do
+    item
+    invoice
+    quantity { Faker::Number.number(digits: 2) }
+    unit_price { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
+  end
+end
