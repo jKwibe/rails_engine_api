a66b8d228c79a3a3f0535f456f4b668765180a8d
Kwibe Merci <jeremiahkwibe@hotmail.com>
2021-11-21 10:18:38 -0700

    Use the filter service

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,11 +7,8 @@ class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
 
-  def self.find_match(query_params)
-    where("lower(#{query_params.keys.first}) LIKE ?", "%#{query_params.values.first.downcase}%").first
-  end
+  scope :filter_by_params, ->(key, value) {
+    where("lower(#{key}) LIKE ?", "%#{value.downcase}%")
+  }
 
-  def self.find_all(query_params)
-    where("lower(#{query_params.keys.first}) LIKE ?", "%#{query_params.values.first.downcase}%")
-  end
 end
