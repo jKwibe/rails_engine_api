431ec92165717ba37072b6ede5ebc5d3447be5c7
Kwibe Merci <jeremiahkwibe@hotmail.com>
2021-11-21 10:17:27 -0700

    Add custom exception

--- app/poros/total_revenue.rb
+++ app/poros/total_revenue.rb
@@ -1,11 +1,13 @@
 class TotalRevenue
   attr_reader :id
-  def initialize(start_time, end_time)
+  def initialize(start_time: , end_time:)
     @start_time = start_time
     @end_time = end_time
+    @id = SecureRandom.uuid
   end
 
   def revenue
-    Invoice.total_revenue(@start_time, @end_time)
+    raise Exceptions::InvalidParams.new('[start] and [end] query params should be defined') if @end_time.nil? || @start_time.nil?
+    Invoice.total_revenue(start: @start_time, finish: @end_time)
   end
 end
