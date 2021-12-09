c42a507ee0a5faf68dbcb2f837b6194b18da26b5
Kwibe Merci <jeremiahkwibe@hotmail.com>
2021-12-08 20:07:08 -0700

    Remove repetition
    yay!!!

--- spec/requests/api/v1/items/items_update_spec.rb
+++ spec/requests/api/v1/items/items_update_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Update' do
-  it 'Updates item' do
-    item_1 = create(:item, unit_price: 33.59)
-    patch "/api/v1/items/#{item_1.id}", params: { unit_price: 30.67, name: 'Colgate'}
+  let(:item) { create(:item, unit_price: 33.59) }
+  let(:params) { { unit_price: 30.67, name: 'Colgate'} }
+  let(:body) { JSON.parse(response.body, symbolize_names: true) }
 
-    data = JSON.parse(response.body, symbolize_names: true)
+  it 'Updates item' do
+    patch "/api/v1/items/#{item.id}", params: params
+
     expect(response.status).to eq(200)
-    expect(data[:data][:attributes][:name]).to eq('Colgate')
-    expect(data[:data][:attributes][:unit_price]).to eq(30.67)
+    expect(body[:data][:attributes][:name]).to eq('Colgate')
+    expect(body[:data][:attributes][:unit_price]).to eq(30.67)
   end
 
   it 'throws an error if an exception' do
-    create(:item, unit_price: 33.59)
-    item_params = { unit_price: 30.67, name: 'Colgate'}
-    patch "/api/v1/items/m99", params: { item: item_params }
+    patch "/api/v1/items/m99", params: params
 
-    error = JSON.parse(response.body, symbolize_names: true)
     expect(response.status).to eq(404)
-    expect(error[:data][:attributes][:message]).to eq('Couldn\'t find Item with \'id\'=m99')
+    expect(body[:data][:attributes][:message]).to eq('Couldn\'t find Item with \'id\'=m99')
   end
 end
